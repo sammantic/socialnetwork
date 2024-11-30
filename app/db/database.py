8fb3b251bbc34af7852500952cfaab406b8085b3
Mohamed Alsamman <sammantic4@gmail.com>
2024-11-30 18:38:40 +0100

    Refactoring: update the imports of database connection & configuration

--- app/db/database.py
+++ app/db/database.py
@@ -1,17 +1,11 @@
-from psycopg2 import sql, connect, Error
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-HOST = "db"
-USER = "admin"
-PASSWORD = "admin"
-PORT = "5432"
-DATABASE_NAME = "socialnetwork"
+from app.core.config import get_database_url
 
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}/{DATABASE_NAME}"
-
+# Database connection URL
+DATABASE_URL = get_database_url()
 
 # Create SQLAlchemy engine and session
 engine = create_engine(DATABASE_URL)
@@ -19,51 +13,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, futu
 
 # Base class for all models
 Base = declarative_base()
-
-
-def create_database_if_not_exists():
-    """
-    Create database if not exist
-    :return:
-    """
-    
-    try:
-        # Connect to the default 'postgres' database
-        connection = connect(
-            dbname="postgres",
-            user=USER,
-            password=PASSWORD,
-            host=HOST,
-            port=PORT
-        )
-
-        # Set autocommit to True to allow CREATE DATABASE to execute outside of a transaction
-        connection.autocommit = True
-
-        cursor = connection.cursor()
-
-        # Check if the database exists
-        cursor.execute(
-            "SELECT 1 FROM pg_database WHERE datname = %s;",
-            (DATABASE_NAME,)
-        )
-        exists = cursor.fetchone()
-
-        if not exists:
-            # Create the database if it doesn't exist
-            cursor.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(DATABASE_NAME)))
-            print(f"Database '{DATABASE_NAME}' created successfully.")
-        else:
-            print(f"Database '{DATABASE_NAME}' already exists.")
-
-    except Error as e:
-        print(f"Error: {e}")
-    finally:
-        # Clean up resources
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
 
 
 # DB Utilities
