573b89934125987df40be405893a841030600a29
Mohamed Alsamman <sammantic4@gmail.com>
2025-02-16 06:07:12 +0100

    Read configs by using Pydantic and remove get_setting, call get config directly one time

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,13 @@
-from app.core.database.config_loader import get_setting
+from app.core.database.config_loader import get_config
 
-DB_HOST = get_setting("database", "host")
-DB_PORT = get_setting("database", "port")
-DB_USER = get_setting("database", "user")
-DB_PASSWORD = get_setting("database", "password")
-DB_NAME = get_setting("database", "name")
+
+config = get_config()
+
+DB_HOST = config.host
+DB_PORT = config.port
+DB_USER = config.user_db
+DB_PASSWORD = config.password
+DB_NAME = config.name
 
 
 def get_database_url(db_name: str = DB_NAME) -> str:
@@ -15,4 +18,5 @@ def get_database_url(db_name: str = DB_NAME) -> str:
     :return:
     str: a database connection url
     """
+
     return f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
--- app/core/database/config_loader.py
+++ app/core/database/config_loader.py
@@ -1,44 +1,37 @@
-import os
-from configparser import ConfigParser
 from pathlib import Path
 from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
+class Settings(BaseSettings):
+    name: str
+    host: str
+    port: int
+    user_db: str
+    password: str
+
+    model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/config/.env")
+
+
 @lru_cache()
-def get_config(environment: str = None) -> ConfigParser:
+def get_config() -> Settings:
     """
     Read a config file base on the environment
     the default environment is dev
 
-    :param environment: environment name
     :return:
     Object of ConfigParser
     """
-    environment = environment or os.getenv("APP_ENV", "dev")
-    config_file = BASE_DIR / f"config/config.{environment}.ini"
+
+    config_file = BASE_DIR / f"config/.env"
 
     if not config_file.exists():  # Check if the configuration file does not exist
         raise FileNotFoundError(f"Configuration file '{config_file}' not found")
 
-    config = ConfigParser()
-    config.read(config_file)
+    config = Settings()
 
     return config
-
-
-def get_setting(section: str, key: str) -> str:
-    """
-    get a value from configParser
-
-    :param section:
-    :param key:
-    :return:
-    str: a config value
-    """
-    config = get_config()
-    return config.get(section, key)
-
-
 
